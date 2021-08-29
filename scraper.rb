1262a5c1c054fc117e3b0870b72647ce7ddb38e9
Alie Stone <helloaliestone@gmail.com>
2021-08-29 11:59:12 -0400

    commented code & refactored to only loop through element nodes once

--- scraper.rb
+++ scraper.rb
@@ -3,21 +3,21 @@ require 'open-uri'
 
 url = "https://www.oyster.com/articles/usa-travel-bucket-list-places-to-visit-before-you-die/"
 
+# defines a method that takes in an argument of a url 
 def scraping(url)
+
+    # uri opens the url and sets a variable for the html contained 
     html = URI.open(url)
+
+    # sets a variable for the html that is parsed by nokogiri
     nokogiri_doc = Nokogiri::HTML(html)
-    final_array = []
 
-    nokogiri_doc.search("h2.heading a").each do |element|
-        element = element.text
-        final_array << element
+    # nokogiri searches html document for the a tag within the heading class and iterates through each instance
+    nokogiri_doc.search("h2.heading a").each_with_index do |element, index|
+        #puts formatted text from the node with index 
+        puts "#{index + 1} - #{element.text}"
     end 
-
-    print final_array
-
-    final_array.each_with_index do |element, index|
-        puts "#{index + 1} - #{element}"
-    end
 end 
 
+#calls method with url argument 
 scraping(url)
